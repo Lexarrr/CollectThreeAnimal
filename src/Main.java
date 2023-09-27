554b4c6e67a4c10d2a6f7cfe4519366966e6e593
Lexa <etc.21@yandex.ru>
2023-09-27 18:48:49 +0300

    My collection with animal

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ import Mammal.Otter;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import static java.lang.StringUTF16.compareTo;
 
 public class Main {
     public static void main(String[] args) {
